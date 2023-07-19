1a82c2906e533c7340c6b0026488e2e1af40da61
Boopathiat07 <ganapathi.boopathi@divum.in>
2023-07-19 12:28:04 +0530

    BranchB Fibinocii edited

--- Fibinociiusingloop.java
+++ Fibinociiusingloop.java
@@ -1,14 +1,16 @@
    public class Fibinociiusingloop {
-        public static void main(String[] args) {
-            System.out.println("Fibonacci Series using for-loop :- ");
-            int num1 = 0, num2 = 1;
-            int limit = 15;
-            for(int i = 0; i < limit; i++) {
-                System.out.print(num1 + " ");
-                int sum = num1 + num2;
-                num1 = num2;
-                num2 = sum;
-            }
-         }
+    public static void main(String[] args) {
+        System.out.println("Fibonacci Series using while-loop :- ");
+        int num1 = 0, num2 = 1;
+        int limit = 15;
+        int iCount = 0;
+        while(iCount < limit) {
+            System.out.print(num1 + " ");
+            int sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+            iCount++;
+        }
+       }
     }
     
